b7e4cba8311b6a750e9ffdb9b64aed11b1681b9f
PaoloBarahona <20240048@ricaldone.edu.sv>
2025-09-17 16:04:59 -0600

    Se arregla el apartado de servicios

--- js/Controller/ServiciosController.js
+++ js/Controller/ServiciosController.js
@@ -1,14 +1,17 @@
-$(document).ready(function(){
-	
-	$('.element-card').on('click', function(){
-		
-		if ( $(this).hasClass('open') ) {
-			$(this).removeClass('open');
-		} else {
-			$('.element-card').removeClass('open');
-			$(this).addClass('open');
-		}
-		
-	});
-	
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".element-card");
+
+  cards.forEach(card => {
+    card.addEventListener("click", (e) => {
+      e.stopPropagation();
+      cards.forEach(c => { if (c !== card) c.classList.remove('open'); });
+      card.classList.toggle("open");
+    });
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!e.target.closest(".element-card")) {
+      cards.forEach(c => c.classList.remove("open"));
+    }
+  }, true);
 });
